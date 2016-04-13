b31e93227512a862c2b95293dd4687c0e61d3843
noushka86 <noushka86@gmail.com>
2016-04-13 13:25:21 -0500

    add search component

--- client/imports/components/HomePageContainer.jsx
+++ client/imports/components/HomePageContainer.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import { connect } from 'param-store';
 import HomePage from './HomePage';
+import Search from './Search';
 
 class HomePageContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log();
+  }
+
   render() {
     const style = {
-      position: 'absolute'
+      position: 'absolute',
     };
-    if (this.props.currentParams.path !== 'homePage') {
-      return null;
+    if (this.props.currentParams.path === 'homePage') {
+      return (
+      <div style={style}>
+        <HomePage {...this.props} />
+      </div>
+      );
+    }
+
+    if (this.props.currentParams.path === 'search') {
+      return (
+        <div>
+          <Search />
+        </div>
+      );
     }
 
     return (
-      <div style={style}>
-        <HomePage {...this.props}/>
-      </div>
-      );
+       <div></div>
+    );
   }
 }
 
---
+++ client/imports/components/Search.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log();
+  }
+
+  render() {
+    return (
+      <div>
+        Search
+      </div>
+    );
+  }
+}
+
+export default Search;
