4b2182efbb568e7b90f723ca158e0a63edfa640f
noushka86 <noushka86@gmail.com>
2016-04-14 14:23:26 -0500

    refactor App component and remove HomePageContainer

--- client/imports/components/App.jsx
+++ client/imports/components/App.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import HomePageContainer from './HomePageContainer';
+import { connect } from 'param-store';
+import HomePage from './HomePage';
+import Search from './Search';
+import ParamStore from 'param-store';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log();
+  }
+
+  componentWillMount() {
+    ParamStore.set({ path: 'homePage' })
+  }
+
   render() {
-    return (
-      <div>
-        <HomePageContainer/>
+    const style = {
+      position: 'absolute',
+    };
+    if (this.props.currentParams.path === 'homePage') {
+      return (
+      <div style={style}>
+        <HomePage {...this.props} />
       </div>
       );
+    }
+
+    if (this.props.currentParams.path === 'search') {
+      return (
+        <div>
+          <Search />
+        </div>
+      );
+    }
+
+    return (
+       <div></div>
+    );
   }
 }
+
+export default connect(App, 'path');
