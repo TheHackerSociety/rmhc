9534ce02a55dcf66bf4640245bcf454f8590621e
noushka86 <noushka86@gmail.com>
2016-04-21 16:21:52 -0500

    commit befor updating design

--- client/imports/components/Search.jsx
+++ client/imports/components/Search.jsx
@@ -1,46 +1,42 @@
 import React from 'react';
 import ParamStore from 'param-store';
-import Geosuggest from 'react-geosuggest';
 
 export default class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log();
-  }
-
-  onSuggestSelect() {
-    ParamStore.set({ path: 'events-by-location' });
-  }
-
   render() {
     return (
       <div className="body-color">
         <div className="container">
           <nav>
-            <a href="index.html"
-              className="nav-text cancel"
-              onClick={e => {
-                ParamStore.set({ path: 'index' });
-                e.preventDefault();
-              }}
+            <a href="index.html" className="nav-text cancel" onClick={e => {
+              ParamStore.set({path: 'index' });
+              e.preventDefault();
+            }}
             >
               Cancel
             </a>
           </nav>
+          <a href="#" className="w-inline-block address-option">
+            <div> 675 Bering St. Houston TX, USA </div>
+          </a>
+          <a href="#" className="w-inline-block address-option">
+            <div> 675 Bering St. Houston TX, USA </div>
+          </a>
+          <a href="#" className="w-inline-block address-option">
+            <div> 675 Bering St. Houston TX, USA </div>
+          </a>
           <div className="address-input">
             <div className="w-form">
               <form id="email-form"
                 name="email-form"
                 data-name="Email Form"
                 className="w-clearfix">
-              <Geosuggest
-                onSuggestSelect={this.onSuggestSelect.bind(this)}
-                inputClassName="w-input input"
-                inputWrapperClassName="address-input"
-                itemClassName="w-inline-block address-option"
-                componentWrapperClassName="container"
-                />
-                 <img alt="target icon" src="images/input-icon.svg" className="address-icon" />
+                <input id="address"
+                  type="text"
+                  placeholder="Address or zip"
+                  name="address"
+                  data-name="address"
+                  className="w-input input" />
+                  <img alt="target icon" src="images/input-icon.svg" className="address-icon" />
               </form>
             </div>
           </div>
@@ -49,12 +45,4 @@ export default class Search extends React.Component {
       );
   }
 }
-
-                //<input id="address"
-                  //type="text"
-                  //placeholder="Address or zip"
-                  //name="address"
-                  //data-name="address"
-                  //className="w-input input"
-                ///>
-
+;
