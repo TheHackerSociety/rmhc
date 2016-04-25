f13eb95eb568fad01bedaf3f3f9792bb8106c89b
noushka86 <noushka86@gmail.com>
2016-04-25 09:17:27 -0500

    hook up webflow to EventsByLocation component and add grouping

--- client/imports/components/EventsByLocation.jsx
+++ client/imports/components/EventsByLocation.jsx
@@ -9,7 +9,8 @@ export default class EventsByLocation extends React.Component {
     this.state = {
       loading: true,
       dists: null,
-      newEvents: null,
+      groupedArray: null,
+      milesGroups: null,
       weekdays: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
       months: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
     };
@@ -40,16 +41,17 @@ export default class EventsByLocation extends React.Component {
     this.getGoogleDistance(origin, destinations, (response, status) => {
       if (status === google.maps.DistanceMatrixStatus.OK) {
         this.setState({ dists: response.rows[0] });
-        this.attachDistanceAndSort();
+        this.attachDistanceSortAndGroup();
       }
     });
   }
 
-  attachDistanceAndSort() {
+  attachDistanceSortAndGroup() {
     const newEvents = _.clone(this.props.events);
     newEvents.forEach((event, index) => {
       event.distance = this.state.dists.elements[index].distance.value;
     });
+
     newEvents.sort((a, b) => {
       if (a.distance > b.distance) {
         return 1;
@@ -59,16 +61,39 @@ export default class EventsByLocation extends React.Component {
       }
       return 0;
     });
-    this.setState({ newEvents, loading: false });
-  }
 
-  showResult(e) {
-    console.log(e.target);
+    const groupedEvents = _.groupBy(newEvents, (event) => {
+      if (event.distance / 1609.344 > 0 && event.distance / 1609.344 <= 10) {
+        return 10;
+      }
+      if (event.distance / 1609.344 > 10 && event.distance / 1609.344 <= 20) {
+        return 20;
+      }
+      return 30;
+    });
+
+
+    const groupedArray = [];
+    const milesGroups = [];
+
+    for (const group in groupedEvents) {
+      if (group === '10') {
+        milesGroups.push('Within 10 miles');
+      } else if (group === '20') {
+        milesGroups.push('Within 20 miles');
+      } else {
+        milesGroups.push('Further than 20 miles');
+      }
+
+      groupedArray.push(groupedEvents[group]);
+    }
+
+    this.setState({ groupedArray, milesGroups, loading: false });
   }
 
   render() {
-        if (this.state.loading) {
-      return(
+    if (this.state.loading) {
+      return (
         <div>Loading</div>
       );
     }
@@ -88,55 +113,61 @@ export default class EventsByLocation extends React.Component {
               Back
             </a>
           </nav>
-          <section>
-            <div className="location-top-info">
-              <div>
-                Within 10 miles
-              </div>
-            </div>
-            {this.state.newEvents.map((event, index) => {
+          {
+            this.state.groupedArray.map((array, index) => {
               return (
-                <div key={index} className="location-card">
-                  <div className="w-clearfix location-card-header">
-                    <div className="w-clearfix miles-container">
-                      <div className="miles-number">
-                        {(event.distance / 1609.344).toFixed(2)}
-                      </div>
-                      <div className="miles-text">
-                        MILES
-                        <br />AWAY
-                      </div>
-                    </div>
-                    <div className="w-clearfix date-container">
-                      <div className="secondary-font-color day">
-                        {this.state.weekdays[event.date.getDay()]}
-                        <br />{this.state.months[event.date.getMonth()]}
-                      </div>
-                      <div className="secondary-font-color day-number">
-                        {event.date.getDate()}
-                      </div>
+                <section key={index}>
+                  <div className="location-top-info">
+                    <div>
+                      {this.state.milesGroups[index]}
                     </div>
                   </div>
-                  <div className="w-clearfix card-info">
-                    <a className="w-inline-block w-clearfix location-icon">
-                      <img alt="location icon" src="images/location-icon.svg" />
-                    </a>
-                    <div className="secondary-font-color location-name">
-                      {event.place}
-                    </div>
-                    <div className="secondary-font-color location-address">
-                      {event.address}
-                    </div>
-                    <div className="secondary-font-color location-time">
-                      {event.morningStartTime}-{event.morningEndTime}
-                      <br />
-                      {event.noonStartTime}-{event.noonEndTime}
-                    </div>
-                  </div>
-                </div>
+                  {array.map((event, index) => {
+                    return (
+                      <div key={index + 3} className="location-card">
+                        <div className="w-clearfix location-card-header">
+                          <div className="w-clearfix miles-container">
+                            <div className="miles-number">
+                              {(event.distance / 1609.344).toFixed(2)}
+                            </div>
+                            <div className="miles-text">
+                              MILES
+                              <br />AWAY
+                            </div>
+                          </div>
+                          <div className="w-clearfix date-container">
+                            <div className="secondary-font-color day">
+                              {this.state.weekdays[event.date.getDay()]}
+                              <br />{this.state.months[event.date.getMonth()]}
+                            </div>
+                            <div className="secondary-font-color day-number">
+                              {event.date.getDate()}
+                            </div>
+                          </div>
+                        </div>
+                        <div className="w-clearfix card-info">
+                          <a className="w-inline-block w-clearfix location-icon">
+                            <img alt="location icon" src="images/location-icon.svg" />
+                          </a>
+                          <div className="secondary-font-color location-name">
+                            {event.place}
+                          </div>
+                          <div className="secondary-font-color location-address">
+                            {event.address}
+                          </div>
+                          <div className="secondary-font-color location-time">
+                            {event.morningStartTime}-{event.morningEndTime}
+                            <br />
+                            {event.noonStartTime}-{event.noonEndTime}
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </section>
               );
-            })}
-          </section>
+            })
+          }
         </div>
       </div>
       );
