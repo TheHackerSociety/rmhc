e8998d3aed29be52b27d98a3b8ab994b01363e90
noushka86 <noushka86@gmail.com>
2016-05-07 15:17:20 -0500

    add field population

--- client/imports/components/admin/EventForm.jsx
+++ client/imports/components/admin/EventForm.jsx
@@ -2,9 +2,36 @@ import React from 'react';
 import MorningSelect from './MorningSelect';
 import AfternoonSelect from './AfternoonSelect';
 import PlaceItemAdmin from './PlaceItemAdmin';
+import GooglePlaces from 'react-google-places-component';
 
 export default class EventForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      place: null,
+      address: null,
+      date: null,
+      morningStart: '9:00 am',
+      morningEnd: '11:15 am',
+      afternoonStart: '1:00 pm',
+      afternoonEnd: '2:00 pm',
+    };
+
+    this.setAddress = this.setAddress.bind(this);
+    this.setTime = this.setTime.bind(this);
+  }
+
+  setAddress(address) {
+    this.setState({ address });
+  }
+
+  setTime(timeObject) {
+    this.setState(timeObject);
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="w-form form-wrapper">
         <div className="w-clearfix main-form">
@@ -13,6 +40,7 @@ export default class EventForm extends React.Component {
               Location Name:
             </label>
             <input id="Location-2"
+              onBlur={(e) => this.setState({ place: e.target.value}) }
               type="text"
               placeholder="Enter Location"
               name="Location-2"
@@ -26,6 +54,7 @@ export default class EventForm extends React.Component {
               Address:
             </label>
             <input id="Address-2"
+              onChange={(e) => this.setState({ query: e.target.value })}
               type="text"
               placeholder="Enter Address"
               name="Address-2"
@@ -34,6 +63,11 @@ export default class EventForm extends React.Component {
               className="w-input text-field"
             />
             <div className="address-popout">
+              <GooglePlaces
+                options={{ input: this.state.query }}
+                itemProps={{ onClick: this.setAddress }}
+                itemComponent={PlaceItemAdmin}
+              />
             </div>
           </div>
           <div className="input-container date-input">
@@ -41,6 +75,7 @@ export default class EventForm extends React.Component {
               Date:
             </label>
             <input id="Date-2"
+              onBlur={(e) => this.setState({ date: e.target.value })}
               type="date"
               placeholder="mm/dd/yyyy"
               name="Date-2"
@@ -53,21 +88,34 @@ export default class EventForm extends React.Component {
             <label htmlFor="field-27">
               Morning Session:
             </label>
-            <MorningSelect />
+            <MorningSelect
+              setTime={this.setTyime}
+              timeKey={"morningStart"}
+              defaultTime={this.state.morningStart}
+            />
             <div className="middle-text">
               to
             </div>
-            <MorningSelect />
+            <MorningSelect
+              setTime={this.setTyime}
+              timeKey={"morningStart"}
+              defaultTime={this.state.morningEnd} />
           </div>
           <div className="w-clearfix input-container date">
             <label htmlFor="field-29">
               Afternoon Session:
             </label>
-            <AfternoonSelect />
+            <AfternoonSelect
+              setTime={this.setTyime}
+              timeKey={"morningStart"}
+              defaultTime={this.state.afternoonStart} />
             <div className="middle-text">
               to
             </div>
-            <AfternoonSelect />
+            <AfternoonSelect
+              setTime={this.setTyime}
+              timeKey={"morningStart"}
+              defaultTime={this.state.afternoonEnd} />
           </div>
           <input type="submit"
             value="Submit"
--- client/imports/components/admin/PlaceItemAdmin.jsx
+++ client/imports/components/admin/PlaceItemAdmin.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-const PlaceItemAdmin = ({}) => (
-  <a href="#" className="w-inline-block address-option">
-    <div> 675 Bering St. Houston TX, USA </div>
-  </a>
-);
+export default class PlaceItemAdmin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setAddress = this.setAddress.bind(this);
+  }
 
+  setAddress(e) {
+    console.log(e.target.innerHTML);
+    this.props.onClick(e.target.innerHTML);
+  }
+
+  render() {
+    const { prediction } = this.props;
+    if (prediction) {
+      return (
+        <a href="#"
+          className="w-inline-block address-option"
+
+        >
+          <div onClick={this.setAddress}>{prediction.description}</div>
+        </a>
+      );
+    }
+    return (<div></div>);
+  }
+}
 export default PlaceItemAdmin;
