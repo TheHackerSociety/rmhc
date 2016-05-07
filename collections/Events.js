a91ae63cd5098d99b90791ab23eae965beccaf5e
Moore <brianmoore180@gmail.com>
2016-05-07 12:43:04 -0500

    'validation'

--- collections/Events.js
+++ collections/Events.js
@@ -1,33 +1,39 @@
 Events = new Mongo.Collection('events');
 
+Events._timeOfDayRegex = /^\s*[0-1]?[0-9]\s*:\s*[0-5][0-9]\s+[AP]M*$/i;
+
 Events.attachSchema(new SimpleSchema({
   place: {
     type: String,
   },
   address: {
     type: Object,
-    blackbox: true
   },
   "address.$.street": {
     type: String,
   },
   "address.$.zip": {
     type: String,
+    regEx: /^\s*\d{5}\s*(-\s*\d{4}\s*)?$/,
   },
   date: {
     type: Date,
   },
   morningStartTime: {
     type: String,
+    regEx: Events._timeOfDayRegex,
   },
   morningEndTime: {
     type: String,
+    regEx: Events._timeOfDayRegex,
   },
   noonStartTime: {
     type: String,
+    regEx: Events._timeOfDayRegex,
   },
   noonEndTime: {
     type: String,
+    regEx: Events._timeOfDayRegex,
   },
 }));
 
