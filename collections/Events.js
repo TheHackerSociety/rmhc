d1cc7994820269fc40cc04960c4d4e4b400a5900
devyn holman <Devyn@devyns-MacBook-Pro.local>
2016-05-07 17:26:42 -0500

    committing all changes

--- collections/Events.js
+++ collections/Events.js
@@ -31,4 +31,18 @@ Events.attachSchema(new SimpleSchema({
   },
 }));
 
+
+Events.attachSchema.labels({
+  place: i18n("Enter Place"),
+  address: i18n("Enter Address"),
+  "address.$.street": i18n("street name"),
+  "address.$.zip": i18n("zip code"),
+  date:i18n("date of event"),
+  morningStartTime: i18n("When the event will begin"),
+  morningEndTime: i18n("When the event will end"),
+  noonStartTime: i18n("noon start time"),
+  noonEndTime: i18n("noon end time")
+});
+
 export default Events;
+
