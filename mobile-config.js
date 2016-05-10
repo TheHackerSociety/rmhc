d3a609f09c28cdf124540039408029152653fd9e
noushka86 <noushka86@gmail.com>
2016-05-09 20:25:08 -0500

    add splash screens and icons

--- mobile-config.js
+++ mobile-config.js
@@ -6,12 +6,30 @@ App.info({
   author: 'PoeticSystems'
 });
 
-// App.icons({
-//   'iphone': 'icons/icon-60.png',
-//   'iphone_2x': 'icons/icon-60@2x.png',
-// });
+App.icons({
+  'iphone': 'resources/icons/ios/app-icon@60x60.png',
+  'iphone_2x': 'resources/icons/ios/app-icon@120x120.png',
+  'iphone_3x': 'resources/icons/ios/app-icon@180x180.png',
+  'ipad': 'resources/icons/ios/app-icon@76x76.png',
+  'ipad_2x': 'resources/icons/ios/app-icon@152x152.png',
 
- //App.launchScreens({
-   //'ipad_landscape_7-8_@1x': 'resources/splash/ipad_landscape_7-8_@1x.png',
-   //'iphone_2x': 'splash/Default@2x~iphone.png',
- //});
+  // Android
+  'android_mdpi': 'resources/icons/android/android-icon-48.png',
+  'android_hdpi': 'resources/icons/android/android-icon-72.png',
+  'android_xhdpi': 'resources/icons/android/android-icon-96.png' });
+
+App.launchScreens({
+  'iphone': 'resources/splash/iphone_portrait_4_@1x.png',
+  'iphone_2x': 'resources/splash/iphone_portrait_4_@2x.png',
+  'iphone5': 'resources/splash/iphone_portrait_5_@2x.png',
+  'iphone6': 'resources/splash/iphone_portrait_6_@2x.png',
+  'iphone6p_portrait': 'resources/splash/iphone_portrait_6p_@3x.png',
+  'iphone6p_landscape': 'resources/splash/splash-736x414@3x.png',
+
+  'ipad_portrait': 'resources/splash/ipad_portrait_7-8_@1x.png',
+  'ipad_portrait_2x': 'resources/splash/ipad_portrait_7-8_@2x.png',
+  'ipad_landscape': 'resources/splash/ipad_landscape_7-8_@1x.png',
+  'ipad_landscape_2x': 'resources/splash/ipad_landscape_7-8_nobar_@2x.png',
+
+  'android_mdpi_portrait': 'resources/splash/iphone_portrait_4_@1x.png',
+ });
