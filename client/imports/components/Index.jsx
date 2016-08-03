944978bc2024b8f99e3c132507043efa836bb56d
Chun Yang <yang2007chun@gmail.com>
2016-08-03 16:47:59 -0500

    fix: make image stretch

--- client/imports/components/Index.jsx
+++ client/imports/components/Index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Link } from 'param-store';
 
+const imageStyle = {
+  backgroundImage: 'url(/images/rmhc-logo.png)',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'contain',
+  backgroundPosition: 'center',
+  marginBottom: '15px',
+  flexGrow: '1',
+};
+
 const Index = ({}) => (
       <div className="body-color">
-        <div className="container">
+        <div className="container" style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
           <nav className="w-clearfix nav">
             <Link href="info.html"
               className="w-inline-block w-clearfix info-icon"
@@ -12,17 +21,8 @@ const Index = ({}) => (
               <img alt="information icon" src="images/information-icon.svg" />
             </Link>
           </nav>
-            <img alt="Ronald McDonald House Charities logo"
-              width="178"
-              src="images/rmhc-logo.svg"
-              className="center"
-            />
-            <img alt="Ronald McDonald Care Mobile "
-              width="280"
-              src="images/care-mobile-logo.png"
-              className="center care-mobile-logo"
-            />
-          <div className="w-clearfix btn-wrapper">
+          <div style={imageStyle}></div>
+          <div className="w-clearfix btn-wrapper" style={{ position: 'relative' }}>
             <Link href="search.html"
               className="w-inline-block btn-container primary-btn-color"
               params={{ path: 'search' }}
